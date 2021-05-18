3fb82053991e13ad87f6404bdc197b8af1cf4341
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-05-18 23:52:24 +0530

    axios test

--- api/date.py
+++ api/date.py
@@ -1,5 +1,4 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
 
 
 class handler(BaseHTTPRequestHandler):
@@ -8,6 +7,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(
-            str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        self.wfile.write("from python")
         return
