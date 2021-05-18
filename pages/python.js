d94ea295ebd7fe514cb7a2e3dd90de464e841803
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-05-18 23:47:12 +0530

    axios test

--- pages/python.js
+++ pages/python.js
@@ -1,18 +1,16 @@
 import Layout from "../components/layout";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Python() {
   const [dateToday, setDateToday] = useState();
   useEffect(() => {
-    fetch("/api/date/", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
+    axios
+      .get("/api/date")
       .then((res) => res.json())
       .then((data) => setDateToday(data));
   }, [dateToday]);
+
   return (
     <Layout title='Testing Python'>
       Fetches data from python
